2e637366804013b2202c4d7807f776074a44e4fe
anaesparapani <anaclaraesparapani@gmail.com>
2025-01-29 11:22:55 -0300

    29-01-2025

---
+++ meuSitebasic/src/App.jsx
@@ -0,0 +1,9 @@
+import Componet1 from "./Componet";
+function App() {
+  return (
+    <div>
+      <Componet1/>
+    </div>
+  );
+}
+export default App;
---
+++ meuSitebutton/src/App.jsx
@@ -0,0 +1,25 @@
+function App() {
+  return (
+    <div>
+      <h1>Bem vindo ao React</h1>
+      <p>Exemplo 2 de componentização</p>
+      <Button/>
+    </div>
+  );
+}
+
+function Button() {
+  function click() {
+    alert("Você clicou na botão");
+  }
+  return (
+    <button
+      style={{ padding: "10px 20px", fontSize: "16px", cursor: "pointer" }}
+      onClick={() =>click()}
+    >
+      Clique aqui
+    </button>
+  );
+}
+
+export default App;
